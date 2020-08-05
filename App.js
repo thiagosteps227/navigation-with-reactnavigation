ae89387b57df4bd3fbac11e4caf12dbf708f8ca2
Thiago dos Passos <64388418+thiagosteps227@users.noreply.github.com>
2020-08-05 10:39:38 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { SafeAreaView, Text } from 'react-native'
+import TelaA from './views/TelaA'
+import TelaB from './views/TelaB'
+import TelaC from './views/TelaC'
+import TelaD from './views/TelaD'
+
+export default props => (
+    <SafeAreaView style={{flex:1}}>
+        <TelaA/>
+        <TelaB/>
+        <TelaC/>
+        <TelaD/>
+    </SafeAreaView>
+)
